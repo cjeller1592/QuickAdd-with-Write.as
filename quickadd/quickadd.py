240010eded5da01a92df9f5da86424efa0f7e501
CJ Eller <45696734+cjeller1592@users.noreply.github.com>
2018-12-10 15:13:45 -0500

    Update comments

--- quickadd/quickadd.py
+++ quickadd/quickadd.py
@@ -4,6 +4,7 @@ from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
 
+# These scopes allow us to add events to our calendar
 # If modifying these scopes, delete the file token.json.
 SCOPES = 'https://www.googleapis.com/auth/calendar.events'
 
@@ -17,15 +18,20 @@ def main():
     store = file.Storage('token.json')
     creds = store.get()
 
+    # Make sure to register your app in the Google developer's console
+    # By doing so you will retrieve your 'credentials.json' file needed to 
     if not creds or creds.invalid:
         flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
         creds = tools.run_flow(flow, store)
     calendar = build('calendar', 'v3', http=creds.authorize(Http()))
 
     # Call the Write.as API to fetch the post
+    # When you run the script in the cmd line, it will ask you for the post id
     headers = {'Content-Type': 'application/json'}
     url = 'https://write.as/api/posts/' + raw_input("Give post id here: ")
     post = requests.get(url, headers=headers)
+    
+    # Once the request is complete, we'll grab the post body
     post_body = post.json()['data']['body']
 
     # Call the Google Calendar API and feed in the post body
